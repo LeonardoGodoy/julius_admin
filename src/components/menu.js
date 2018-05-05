4cc560ab898c4973c2e6cc0cd59ab69390d46bbf
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2018-05-04 21:06:54 -0300

    Atualiza links do menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -18,15 +18,16 @@ export default class MenuExample extends Component {
             <Menu.Item>
               <Image src='/assets/images/logo.png' size='small' />
             </Menu.Item>
-            <Menu.Item as={Link} to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-            <Menu.Item as={Link} to='/customers' name='customers' active={activeItem === 'customers'} onClick={this.handleItemClick} />
-            <Menu.Item as={Link} to='/sales' name='sales' active={activeItem === 'sales'} onClick={this.handleItemClick} />
+            <Menu.Item as={Link} to='/' content='Início' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+            <Menu.Item as={Link} to='/customers' content='Clientes' name='customers' active={activeItem === 'customers'} onClick={this.handleItemClick} />
+            <Menu.Item as={Link} to='/products' content='Produtos' name='products' active={activeItem === 'products'} onClick={this.handleItemClick} />
+            <Menu.Item as={Link} to='/sales' content='Vendas' name='sales' active={activeItem === 'sales'} onClick={this.handleItemClick} />
             <Menu.Menu position='right'>
               <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
             </Menu.Menu>
           </Container>
         </Menu>
-    </div>
+      </div>
     )
   }
 }
